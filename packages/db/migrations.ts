747dda30c48a81c02bc5632b99b8e959dc59e2f8
Alexandre Gambier (Ajustor) <alexandre.gambier78@gmail.com>
2025-05-23 21:50:56 +0200

    Feat(character-and-bestiary): adding log

--- packages/db/migrations.ts
+++ packages/db/migrations.ts
@@ -3,4 +3,6 @@ import { getDB } from './src'
 
 const url: string = `postgresql://${Bun.env.DATABASE_USER}:${Bun.env.DATABASE_PASSWORD}@${Bun.env.DATABASE_HOST}:${Bun.env.DATABASE_PORT}/${Bun.env.DATABASE_NAME}`
 
+console.log('the url', url)
+
 await migrate(getDB(url), { migrationsFolder: './drizzle' })
