af87603300a99a9fa2ad36d671b03ee56491199a
Alexandre Gambier (Ajustor) <alexandre.gambier78@gmail.com>
2025-05-23 22:34:51 +0200

    Feat(character-and-bestiary): remove database name

--- packages/db/migrations.ts
+++ packages/db/migrations.ts
@@ -1,7 +1,7 @@
 import { migrate } from 'drizzle-orm/postgres-js/migrator'
 import { getDB } from './src'
 
-const url: string = `postgresql://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`
+const url: string = `postgresql://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/`
 
 console.log('the url', url)
 
