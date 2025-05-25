2506d27f4245e2b5ee1811ab5ca8a0b23d0191de
Alexandre Gambier (Ajustor) <alexandre.gambier78@gmail.com>
2025-05-25 17:26:24 +0200

    Master(master): adding logs

--- apps/front/src/routes/+layout.server.ts
+++ apps/front/src/routes/+layout.server.ts
@@ -9,6 +9,7 @@ import type { LayoutServerLoad } from './$types'
 export const load: LayoutServerLoad = async ({ cookies, url }) => {
   const isLogged = !!cookies.get('auth')
   const authToken = cookies.get('auth')
+  console.log(authToken, isLogged)
   let user: User | null = null
   if (isLogged && authToken) {
     try {
