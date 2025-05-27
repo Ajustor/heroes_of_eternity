1a88efb524c12f6dbd27fadd6eecdcbc9b04d572
Alexandre Gambier (Ajustor) <alexandre.gambier78@gmail.com>
2025-05-27 06:21:36 +0200

    Feat(beastiary): remove headers

--- apps/front/src/services/ws/chat.ts
+++ apps/front/src/services/ws/chat.ts
@@ -5,11 +5,7 @@ export type ChatApi = ReturnType<typeof wsClient.chat['subscribe']>
 export function connectChat(token: string) {
     console.log('Connecting chat system', token)
 
-    const chat = wsClient.chat.subscribe({
-        headers: {
-            authorization: `Bearer ${token}`
-        }
-    })
+    const chat = wsClient.chat.subscribe()
 
     console.log('Chat connected')
     return chat
