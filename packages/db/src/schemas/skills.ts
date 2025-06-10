eb21b1872557a5783e86ce1a7ab66591db0100ae
Alexandre Gambier (Ajustor) <alexandre.gambier78@gmail.com>
2025-06-10 13:31:16 +0200

    Feat(quests): start adding skills

---
+++ packages/db/src/schemas/skills.ts
@@ -0,0 +1,23 @@
+import { pgTable, text, integer, pgEnum } from 'drizzle-orm/pg-core'
+import { createId } from '@paralleldrive/cuid2'
+import { createInsertSchema } from 'drizzle-typebox'
+import { SKILLS_KEYS } from "@hoe/assets"
+import { charactersJobEnum } from './character'
+
+export const skillsSkinEnum = pgEnum('skills_skin', [SKILLS_KEYS.HEAL, SKILLS_KEYS.BLOW, SKILLS_KEYS.DARKNESS])
+
+export const skillsTable = pgTable('skills_table', {
+    id: text('id').primaryKey().$defaultFn(createId).notNull(),
+    name: text('name').notNull(),
+    description: text('description').notNull(),
+    damages: integer().notNull(),
+    cost: integer().notNull(),
+    animation: skillsSkinEnum().notNull(),
+    requiredLevel: integer('required_level').notNull(),
+    job: charactersJobEnum().notNull(),
+})
+
+export type SkillEntity = typeof skillsTable.$inferSelect
+export type SkillCreation = typeof skillsTable.$inferInsert
+
+export const createSkill = createInsertSchema(skillsTable)
