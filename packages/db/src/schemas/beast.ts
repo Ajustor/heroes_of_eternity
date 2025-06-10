ac757edcea48a8734de531052d76c2da64cabc4b
Alexandre Gambier (Ajustor) <alexandre.gambier78@gmail.com>
2025-06-10 13:34:43 +0200

    Feat(quests): add migrations and skills to beasts

--- packages/db/src/schemas/beast.ts
+++ packages/db/src/schemas/beast.ts
@@ -2,6 +2,8 @@ import { integer, pgEnum, pgTable, text, boolean } from 'drizzle-orm/pg-core'
 import { createInsertSchema } from 'drizzle-typebox'
 import { createId } from '@paralleldrive/cuid2'
 import { BEASTS_KEYS, BOSSES_KEYS } from '@hoe/assets'
+import { relations } from 'drizzle-orm'
+import { skillsTable } from './skills'
 
 export const beastsSkinEnum = pgEnum('beast_skin', [BEASTS_KEYS.Bat, BEASTS_KEYS.Rat, BEASTS_KEYS.Skeleton, BEASTS_KEYS.Slime, BEASTS_KEYS.Snake, BEASTS_KEYS.Soldier, BEASTS_KEYS.Spider, BOSSES_KEYS.Chaos, BOSSES_KEYS.Troll, BOSSES_KEYS.DarkLord])
 
@@ -18,6 +20,10 @@ export const beastsTable = pgTable('beast_table', {
   isBoss: boolean('is_boss').default(false).notNull(),
   experience: integer().default(1).notNull(),
 })
+
+export const beastSkillsRelations = relations(beastsTable, ({ many }) => ({
+  skills: many(skillsTable),
+}))
 
 export type BeastEntity = typeof beastsTable.$inferSelect
 export type BeastCreation = typeof beastsTable.$inferInsert
