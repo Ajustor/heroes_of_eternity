92cb86fc1d9b8a199bf6fd8864c40596c511892c
Alexandre Gambier (Ajustor) <alexandre.gambier78@gmail.com>
2025-06-24 09:18:46 +0200

    Master(master): split code

--- apps/sockets/src/index.ts
+++ apps/sockets/src/index.ts
@@ -1,37 +1,12 @@
 import { createServer } from "http";
 import { Server } from "socket.io";
-import { getUser } from "./api/user-api";
+import { linkChatSystem } from "./services/chat";
 
 const httpServer = createServer();
 const io = new Server(httpServer, { cors: { origin: ['https://hoe.darthoit.eu', 'http://localhost:8080'] } });
 
 io.on('connect', (socket) => {
-    socket.on('helloThere', async (body) => {
-
-        if (body.accessToken) {
-            const user = await getUser(body.accessToken)
-            if (!user) {
-                console.info('User is not authenticated start authentication process')
-                socket.send({ message: 'NOT_AUTHENTICATED', username: 'Server' })
-                return
-            }
-
-            socket.data = { ...socket.data, user }
-        }
-
-        socket.broadcast.emit('message', { message: body.message, username: socket.data.user?.username })
-        socket.emit('message', { message: body.message, username: socket.data.user?.username })
-    })
-
-    socket.on('message', (body) => {
-        if (!socket.data.user) {
-            socket.send({ message: 'NOT_AUTHENTICATED', username: 'Server' })
-            return
-        }
-
-        socket.broadcast.emit('message', { message: body.message, username: socket.data.user?.username })
-        socket.emit('message', { message: body.message, username: socket.data.user?.username })
-    })
+    linkChatSystem(socket)
 })
 
 io.on('disconnect', (socket) => {
